69997383018c90e2108e85989e36c6e794fb4d6d
San23042001 <shettysanjay719@gmail.com>
2026-02-23 13:47:51 +0530

    model trainer pipeline completed

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from src.logger import logging
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
-from src.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig
-
+from src.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 
 if __name__ == '__main__':
@@ -29,6 +29,15 @@ if __name__ == '__main__':
         data_transformation_artifact = data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info('Data Transformation Compelted')
+
+        logging.info("Model Training started")
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+
+        model_trainer_artifact = model_trainer.initate_model_trainer()
+
+        print(model_trainer_artifact)
+        logging.info("Model Training artifact created")
         
 
 
--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -19,3 +19,15 @@ class DataTransforamtionArtifacts:
     transformed_object_file_path:str
     transformed_train_file_path:str
     transformed_test_file_path:str
+
+@dataclass 
+class ClassificationMetricArtifact:
+    f1_score:float
+    precision_score: float
+    recall_score: float
+
+@dataclass
+class ModelTrainerArtifact:
+    trainer_model_file_path:str
+    train_metric_artifact:ClassificationMetricArtifact
+    test_metric_artifact:ClassificationMetricArtifact
